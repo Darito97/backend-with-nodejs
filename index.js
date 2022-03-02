83e5c1f3488121aac9c14a24c3b41c01b94faa6b
Darito <davidalonso991@gmail.com>
2022-03-02 15:06:22 -0600

    Creado de servidor basico con node

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hi')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log('server runing on port ' + PORT)
