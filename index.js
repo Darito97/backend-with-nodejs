19effeb1af9a997af3344b6cf03aeb976e427fd6
Darito <davidalonso991@gmail.com>
2022-03-02 15:10:51 -0600

    Muestreo de notes

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require('http')
 
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hi')
+  res.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
