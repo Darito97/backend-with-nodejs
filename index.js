a926a1250d5f51e26b54d7c9aac79c9f426dd1ae
Darito <davidalonso991@gmail.com>
2022-03-04 15:10:10 -0600

    Creado de API REST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -30,6 +31,61 @@ app.get('/graunt', (req, res) => {
 })
 app.get('/api/notes', (req, res) => {
   res.json(notes)
+})
+app.get('/api/notes/:id', (req, res) => {
+  let id = Number(req.params.id)
+  let foundNote = notes.filter(note => note.id === id)
+  if (foundNote.length !== 0) {
+    res.json(foundNote)
+  }
+  else {
+    res.status(404).send('Recurso no encontrado')
+  }
+})
+
+app.post('/api/notes', (req, res) => {
+  let id = notes[notes.length - 1].id + 1
+  const { content, important } = req.body
+  if (content) {
+    let note = {
+      id,
+      content,
+      date: new Date(),
+      important: important || false
+    }
+    notes.push(note)
+    res.status(201).json({ note, success: true })
+  }
+  else {
+    res.status(400).json({ success: false, error: "no-content" })
+  }
+})
+app.delete('/api/notes/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (notes.find(note => note.id === id)) {
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).send('Recurso eliminado')
+  }
+  else {
+    res.status(404).send('Recurso no encontrado')
+  }
+})
+app.put('/api/notes/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (notes.find(note => note.id === id)) {
+    let { content, important } = req.body
+    let index = notes.findIndex(note => note.id === id)
+    notes[index] = {
+      id,
+      content,
+      date: new Date(),
+      important
+    }
+    res.status(200).json({ changed: true })
+  }
+  else {
+    res.status(404).json({ changed: false, error: 'not found' })
+  }
 })
 
 const PORT = 3001
